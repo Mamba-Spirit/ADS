abc495d6f123116a883690d3c8fcd1946f5165a8
Mamba-Spirit <axellngandu@gmail.com>
2024-12-11 14:43:47 +0100

    basics functions for stack with comments

--- stack/stack.h
+++ stack/stack.h
@@ -12,16 +12,16 @@ typedef struct STACK
 
 } STACK;
 
-STACK *new_stack(void);
-bool is_stack_empty(STACK *stack);
-STACK *push_stack(STACK *stack, int data);
-STACK *clear_stack(STACK *stack);
-void print_stack(STACK *stack);
-STACK *pop_stack(STACK *stack);
-STACK *pop_stack_n_elements(STACK *stack, int nombre_elements);
-STACK *push_stack_n_elements(STACK *stack, int nombre_elements);
-int count_stack_elements(STACK *stack);
-int Top_of_stack(STACK *stack);
-int Bottom_of_stack(STACK *stack);
+STACK *new_stack(void);                                          // creat a empty stack (new)
+bool is_stack_empty(STACK *stack);                               // test if a stack is empy
+STACK *push_stack(STACK *stack, int data);                       // Add an element in the stack
+STACK *clear_stack(STACK *stack);                                // Clean the stack
+void print_stack(STACK *stack);                                  // Prints the elements of a stack
+STACK *pop_stack(STACK *stack);                                  // Remove an element from a stack
+STACK *pop_stack_n_elements(STACK *stack, int nombre_elements);  // Remove n elements from a stack
+STACK *push_stack_n_elements(STACK *stack, int nombre_elements); // Add n elements to a stack
+int count_stack_elements(STACK *stack);                          // Return the numbre of element in a stack
+int Top_of_stack(STACK *stack);                                  // Return the last element added (on top) to a stack
+int Bottom_of_stack(STACK *stack);                               // Return tht firdt element added to a stack
 
 #endif
